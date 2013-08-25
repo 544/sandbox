061c5a3bfe84b14a9cd8ca3b9f30d12c02dec45a
masato <masato.t@gmail.com>
2013-08-25 23:07:21 +0900

    空入力時はなにも表示しない。

--- perl/stdinSample.pl
+++ perl/stdinSample.pl
@@ -2,7 +2,10 @@
 
 print "please stop for CTRL-D\n";
 while( $value = <STDIN> ) {		# 標準入力から文字を読み込む
-	chomp($value); 			# 改行文字を除去
-	print "hello $value \n";	# 出力する
+	if ( $value =~ /\w+/ ) {
+		chomp($value); 			# 改行文字を除去
+		# 出力する
+		print "hello $value \n"; 
+	}
 }
 
