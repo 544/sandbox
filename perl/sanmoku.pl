f96a7fcc2d37834a26e42a064a60b9acf7c6ee4a
masato <masato.t@gmail.com>
2013-08-28 14:47:08 +0900

    debugを関数化してみた

--- perl/sanmoku.pl
+++ perl/sanmoku.pl
@@ -22,14 +22,16 @@ print <<EOD
 EOD
 ;
 
+# カーソル位置
 my ($x, $y) = (1, 1);
+# マス目の座標
 my @field = ( [ "2,2", "2,4", "2,6" ], ["4,2", "4,4", "4,6"], ["6,2", "6,4", "6,6"] );
+# マス目の状態
+my @state = ( [ "","","" ], ["","",""], ["", "", ""] );
 
 for(;;) {
 
-	moveCursor 9,9;
-	printf $i++;
-	moveCursor eval($field[$x][$y]);
+	debug (9,9,$i++);
 	
 	my($R, $U, $V, $Z, $S, $T);
 
@@ -58,10 +60,27 @@ for(;;) {
 		$y -- if $y > 0;
 		moveCursor eval($field[$x][$y]);
 	} elsif ($Z eq " ") {
+		$state[$x][$y] = "O";
+		moveCursor eval($field[$x][$y]);
 		print "O";
 	} elsif ($Z eq "q") {
 		last "good by!";
 	}
+
+	# 状態チェック
+	if ($state[0][0] != "" && $state[0][0] == $state[0][1] && $state[0][1] == $state[0][2] ) {
+		die "clear";
+	}
 }
 
+sub debug {
+	my $i = shift;
+	my $j = shift;
+	my $text = shift;
+	moveCursor $i,$j;
+	printf $text;
+	moveCursor eval($field[$x][$y]);
+}
 
+# ターミナルの挙動を戻す。
+system "stty sane";
