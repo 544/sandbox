6aab24c6e7ae0f39840f8875bfb4caf3f5be2a6a
masato <masato.t@gmail.com>
2014-01-24 08:31:53 +0900

    add

---
+++ perl/ThreadSample.pl
@@ -0,0 +1,36 @@
+use Config;
+$Config{useithreads} || die('Recompile Perl with threads to run this program.');
+
+use strict;
+use warnings;
+
+use threads;
+use Thread::Queue;
+
+sub check_num { 
+	my ($upstream, $cur_prime) = @_;
+	my $kid;
+	my $downstream = Thread::Queue->new();
+
+	while (my $num = $upstream->dequeue()) {
+		next unless ($num % $cur_prime);
+		if ($kid) {
+			$downstream->enqueue($num);
+		} else {
+			print ("Found prime : $num \n");
+			$kid = threads->create(\&check_num, $downstream, $num);
+			if (! $kid) {
+				warn("sorry. run out of threads\n");
+				last;
+			}
+		}
+	}
+	if ($kid) {
+		$downstream->enqueue(undef);
+		$kid->join();
+	}
+}
+
+my $stream = Thread::Queue->new(3..100, undef);
+check_num($stream, 2);
+
