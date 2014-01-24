4cc61a6b05df556b4ebf871e76f56a20a9d5bc01
masato <masato.t@gmail.com>
2014-01-25 00:42:56 +0900

    add comment

--- perl/Daemon.pl
+++ perl/Daemon.pl
@@ -1,14 +1,21 @@
+# Httpデーモンのサンプル
 use HTTP::Daemon;
 use HTTP::Status;
 
+# 引数でport指定
 my $d = HTTP::Daemon->new(
 	LocalPort => 10080,
 )|| die;
 print "please contact me at <URL:", $d->url, ">\n";
 
+# ポーリング処理
 while (my $c = $d->accept) {
+
+	# リクエストがあった場合
 	while (my $r = $c->get_request) {
-		if ($r->method eq 'GET' and $r->url->path eq "/xyzzy" ) {
+		# HTTPメソッドとurl
+		if ($r->method eq 'GET' 
+				and $r->url->path eq "/xyzzy" ) {
 			$c->send_file_response("/etc/passwd");
 		}
 		else {
