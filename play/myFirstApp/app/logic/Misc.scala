5acf5268c75375bf04ce53506e8ae5bb51e6520e
masato <masato.t@gmail.com>
2014-02-22 21:25:32 +0900

    add simple specs2 test

---
+++ play/myFirstApp/app/logic/Misc.scala
@@ -0,0 +1,12 @@
+package logic
+
+object Misc {
+  def hello:String = {
+   return "hello world"; 
+  }
+
+  // フィボナッチ数列を返す
+  def fib(n:Int):Int = {
+    return 0;
+  }
+}
---
+++ play/myFirstApp/test/logic/MiscSpec.scala
@@ -0,0 +1,18 @@
+import org.specs2._
+import specification._
+import logic._;
+
+// call this for ...
+// # test-only MiscSpec
+// at play
+class MiscSpec extends script.Specification with Grouped {
+  def is = s2"""
+  test for Misc.hello
+   + Misc.hello は 'hello world' 文字列を返す。
+  """
+
+  "test for Misc.hello" - new group {
+    eg := Misc.hello mustEqual "hello world";
+  }
+
+}
