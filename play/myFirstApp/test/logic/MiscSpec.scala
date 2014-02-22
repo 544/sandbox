8d3a92ba594ca63aba56149d5f97e6b69a2691d2
masato <masato.t@gmail.com>
2014-02-22 21:28:24 +0900

    add test case
    記述順でケースを判断してるみたい。
    戻り値の長さをかえずに内容だけ変えると。。。
    
    [info] MiscSpec
    [info]   test for Misc.hello
    [info]    + Misc.hello は 11 文字列長を返す。
    [info]    x Misc.hello は 'hello world' 文字列を返す。
    [error]  'hello worlx' is not equal to 'hello world' - g1.e2
    (MiscSpec.scala:17)

--- play/myFirstApp/test/logic/MiscSpec.scala
+++ play/myFirstApp/test/logic/MiscSpec.scala
@@ -8,10 +8,12 @@ import logic._;
 class MiscSpec extends script.Specification with Grouped {
   def is = s2"""
   test for Misc.hello
+   + Misc.hello は 11 文字列長を返す。
    + Misc.hello は 'hello world' 文字列を返す。
   """
 
   "test for Misc.hello" - new group {
+    eg := Misc.hello must have size(11);
     eg := Misc.hello mustEqual "hello world";
   }
 
