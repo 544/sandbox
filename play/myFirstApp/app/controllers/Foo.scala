698c3cf76d4c8923e9e243f8f8e25e3a130892d4
masato <masato.t@gmail.com>
2014-02-22 17:27:20 +0900

    add post sample

--- play/myFirstApp/app/controllers/Foo.scala
+++ play/myFirstApp/app/controllers/Foo.scala
@@ -8,4 +8,12 @@ object Foo extends Controller {
   def index = Action {
     Ok(views.html.foo("変数"));
   }
+
+  def add = Action {
+    req => {
+      val num1 = req.body.asFormUrlEncoded.get("num1")(0).toInt;
+      val num2 = req.body.asFormUrlEncoded.get("num2")(0).toInt;
+      Ok( views.html.add( num1 + num2 ) );
+    }
+  }
 }
