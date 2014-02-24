706a0094a577fc3435e57eb8402a57f3c356ec55
Masato Tsukagoshi <masato.tsukagoshi@O-02935-MAC.local>
2014-02-24 14:54:51 +0900

    optimize fib code
    メモ探索。
    再帰検索の結果をキャッシュしながら検索することでパフォーマンス向上
    n=40のテスト実行時間が
    521 ms => 29 ms に改善した。

--- play/myFirstApp/app/logic/Misc.scala
+++ play/myFirstApp/app/logic/Misc.scala
@@ -5,11 +5,14 @@ object Misc {
    return "hello world"; 
   }
 
+
+  val fibMemo = new Array[Int](1000);
   // フィボナッチ数列を返す
   // a(0) = 0,a(1) = 1 a(n) = a(n-1) + a(n-2)
   def fib(n:Int):Int = {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    return fib(n-1) + fib(n-2);
+    if (n <= 1) return n;
+    if (fibMemo(n) != 0) return fibMemo(n);
+    fibMemo(n) = fib(n-1) + fib(n-2);
+    return fibMemo(n);
   }
 }
