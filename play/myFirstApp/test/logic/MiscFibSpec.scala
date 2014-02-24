5394388f5916335ee75f42ee54ce90583715d979
Masato Tsukagoshi <masato.tsukagoshi@O-02935-MAC.local>
2014-02-24 14:32:17 +0900

    add test cace

--- play/myFirstApp/test/logic/MiscFibSpec.scala
+++ play/myFirstApp/test/logic/MiscFibSpec.scala
@@ -5,13 +5,13 @@ import logic._;
 class MiscFibSpec extends script.Specification with Grouped {
   def is = s2"""
   test for Misc.fib
-   + a(0)  は 0
-   + a(1)  は 1
-   + a(2)  は 1
-   + a(3)  は 2
-   + a(4)  は 3
-   + a(10) は 55
-   + a(30) は 832040
+   + a(0)  は 0      ${Misc.fib(0) === 0}
+   + a(1)  は 1      ${Misc.fib(1) === 1}
+   + a(2)  は 1      ${Misc.fib(2) === 1}
+   + a(3)  は 2      ${Misc.fib(3) === 2}
+   + a(4)  は 3      ${Misc.fib(4) === 3}
+   + a(10) は 55     ${Misc.fib(10) === 55}
+   + a(30) は 832040 ${Misc.fib(30) === 832040}
   """
 
 }
