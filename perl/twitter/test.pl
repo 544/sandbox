821133d3c74d44c8ec8b3cc00a74e2cbe5b7a9fc
Masato Tsukagoshi <masato.tsukagoshi@O-02935-MAC.local>
2014-03-20 19:18:57 +0900

    add twitter api sample

---
+++ perl/twitter/test.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Net::Twitter::Lite::WithAPIv1_1;
+use Data::Dumper;
+use utf8;
+use Encode;
+use YAML;
+
+# https://apps.twitter.com/ に定義したTwitter API 接続情報
+my $conf = YAML::LoadFile(".auth");
+
+my %CONSUMER_TOKENS = (
+	consumer_key => $conf->{consumer_key},
+	consumer_secret => $conf->{consumer_secret},
+	access_token => $conf->{access_token},
+	access_token_secret => $conf->{access_token_secret},
+	apiurl => 'https://api.twitter.com/1.1',
+	ssl => 1,
+	legacy_lists_api=>0,
+);
+
+#API生成
+#NOTE これがないと
+# 500: Can't verify SSL peers without knowing which Certificate Authorities to trust
+# でエラー
+$ENV{'PERL_LWP_SSL_VERIFY_HOSTNAME'} = 0;
+my $twi = Net::Twitter::Lite::WithAPIv1_1->new(%CONSUMER_TOKENS);
+
+my $search_str = "ガンロワ";
+my $res = $twi->search(
+	{
+		q => $search_str,
+		lang => 'ja',
+		#since_id => 446556759335768064,
+	}
+);
+
+my $id_prefix = 'http://twitter.com/#!/ID/status/';
+
+my %hs = %{$res};
+
+foreach my $kk(keys %hs){
+		print $kk."\t".$hs{$kk}."\n";
+}
+
+my @search_res = @{$hs{'statuses'}};
+foreach my $rr (@search_res){
+	print $rr->{created_at}."\n";
+	print encode('utf-8',$rr->{text})."\n";
+	print $id_prefix.$rr->{id}."\n";
+	print "\n";
+}
+
