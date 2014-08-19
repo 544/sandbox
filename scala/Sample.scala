04eda5915633b449a403d594bb33a45629c51bef
Masato Tsukagoshi <masato.tsukagoshi@O-02935-MAC.local>
2014-08-19 18:36:32 +0900

    data format logic
    ソートしたりフィルタリングしたり

--- scala/Sample.scala
+++ scala/Sample.scala
@@ -5,16 +5,29 @@ object Sample extends App {
   val lines = Source.fromFile("dat.csv").getLines.toList
 
   // 各行をカンマで区切った後、タプルにする。要素数が合わないと無視。
-  val coords = lines.collect(_.split(",") match {
+  // No, 名前, AP, Goldでデータが入ってる。
+  val records = lines.collect( _.split(",") match {
       case Array(s1, s2, s3, s4) => (s1, s2, s3, s4)
     });
 
-  //println (stringOf(coords) )
+  // ヘッダを除去する。
+  val quests = if (records.length > 0) records.drop(1) else records;
+  
+  // APでグルーピングしてみる。
+  val grouped = quests.groupBy(n => n._3).values;
 
-  coords foreach { t => println(t._2) } ;
+  // Gold最大のリストにする。
+  // (なんかデータがおかしいのでAP0は弾く) 
+  val maxed = grouped.map{ e => e.maxBy(_._4)}.toList.filter(_._3.toInt != 0);
 
-  //val splited = lines.flatMap{ line => line.split(",|\n") };
-  //splited foreach println
+  // Sortする
+  val hoge = maxed.sortBy( _._3.toInt);
+
+
+  // 表示してみる。
+  //hoge foreach { t => println(t._3) } ;
+  hoge foreach { println }
+
 }
 
 Sample.main(args);
