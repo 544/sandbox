c67eff4ee63d125df45a37e4cdbb581c1a747985
Masato Tsukagoshi <masato.tsukagoshi@O-02935-MAC.local>
2014-08-20 17:29:03 +0900

    add knapsack logic
    ナップサック問題の基本的解法を使って保持APで獲得できるGoldの最適値を返す
    。

--- scala/Sample.scala
+++ scala/Sample.scala
@@ -6,27 +6,35 @@ object Sample extends App {
 
   // 各行をカンマで区切った後、タプルにする。要素数が合わないと無視。
   // No, 名前, AP, Goldでデータが入ってる。
-  val records = lines.collect( _.split(",") match {
-      case Array(s1, s2, s3, s4) => (s1, s2, s3, s4)
+  // 一行目はヘッダなので除去する。
+  val records = lines.drop(1).collect( _.split(",") match {
+      case Array(s1, s2, s3, s4) => (s1.toInt, s2, s3.toInt, s4.toInt)
     });
 
-  // ヘッダを除去する。
-  val quests = if (records.length > 0) records.drop(1) else records;
-  
   // APでグルーピングしてみる。
-  val grouped = quests.groupBy(n => n._3).values;
+  val grouped = records.groupBy(n => n._3).values;
 
   // Gold最大のリストにする。
   // (なんかデータがおかしいのでAP0は弾く) 
   val maxed = grouped.map{ e => e.maxBy(_._4)}.toList.filter(_._3.toInt != 0);
 
   // Sortする
-  val hoge = maxed.sortBy( _._3.toInt);
+  val qlist = maxed.sortBy( _._3.toInt);
 
+  //  チェックする基準値は標準入力から受け取る
+  val target = if (args.length > 0) args(0).toInt else 9;
+
+
+  // ボトムアップ型のナップサック問題解法。Gold取得のｓ最適値をだす。
+  val solved = new Array[Int](target+1);
+  for (ap <- 1 to target) {
+    solved(ap) = qlist.map {
+      g => if (ap < g._3) 0 else solved(ap - g._3) + g._4
+    }.reduceLeft((a:Int,b:Int) => Math.max(a,b))
+  };
 
   // 表示してみる。
-  //hoge foreach { t => println(t._3) } ;
-  hoge foreach { println }
+  println { solved(target) }
 
 }
 
